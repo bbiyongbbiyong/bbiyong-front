88331ebc563e187c35b79d20f90c939c28d59cd3
denose0120 <denose0120@gmail.com>
2022-12-18 14:10:23 +0900

    feat: connect server

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -5,7 +5,7 @@ import MsgBox from './MsgBox.js';
 export default function CheckBox() {
   const title = [ 
     {id: 0, name: '재난문자', path: 'emerMsg'},
-    {id: 1, name: '지하철정보', path: 'twitter'},
+    {id: 1, name: '지하철정보', path: 'metro'},
     {id: 2, name: '도로통제정보', path: 'accident'},
   ];
 
