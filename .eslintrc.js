c38e7134e113b6257cd9e5dd1ec240c067b6be7f
nijuy <youjin6325@naver.com>
2023-07-11 01:10:24 +0900

    chore: eslint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   extends: [
     'airbnb-base',
     'plugin:import/recommended',
@@ -7,6 +12,9 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': ['error'],
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'react/prop-types': 'off',
     'import/order': [
       'error',
       {
