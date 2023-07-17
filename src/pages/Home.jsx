b050ac443b8cc5a16cba02199f71595e4f317521
nijuy <youjin6325@naver.com>
2023-07-18 00:09:22 +0900

    chore(Home): 서비스 로고 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Logo from '../assets/serviceLogo.svg';
 import CheckBox from '../components/CheckBox';
 import SeoulMap from '../components/SeoulMap';
 
@@ -8,10 +9,14 @@ import '../css/Home.css';
 const Home = () => {
   return (
     <div>
-      <h4 id="service-title">
-        실시간 재난/교통 정보 모아보기&nbsp;
-        <span>삐용삐용</span>
-      </h4>
+      <div id="title-container">
+        <img src={Logo} id="logo" />
+        <h4 id="service-title">
+          실시간 재난/교통 정보 모아보기&nbsp;
+          <span>삐용삐용</span>
+        </h4>
+      </div>
+
       <SeoulMap />
       <CheckBox />
     </div>
