bf7469b006e1ffbf6bab51d7d81c32214ee0e3ae
nijuy <youjin6325@naver.com>
2023-07-18 21:07:29 +0900

    feat: SigninModal 컴포넌트 생성

---
+++ src/components/SigninModal.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+import closeButton from '../assets/closeButton.svg';
+
+import '../css/SigninModal.css';
+
+const SigninModal = ({ closeSigninModal }) => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const onClickCloseButton = () => closeSigninModal();
+
+  const handleID = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePW = (e) => {
+    setPw(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(`${id} / ${pw}로 로그인 시도`);
+  };
+
+  return (
+    <div
+      id="signin-modal-container"
+      onClick={(e) => {
+        if (e.target.id === 'signin-modal-container') {
+          e.stopPropagation();
+          onClickCloseButton();
+        }
+      }}
+    >
+      <div id="signin-modal">
+        <button id="signin-modal-close" onClick={onClickCloseButton}>
+          <img src={closeButton} alt="" />
+        </button>
+
+        <div id="signin-content">
+          <h4> Login </h4>
+          <form onSubmit={onSubmit}>
+            <input placeholder="id" onChange={handleID} />
+            <input placeholder="pw" onChange={handlePW} />
+
+            <div>
+              <a href="/join"> 회원가입 </a>
+              <button> 확인 </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SigninModal;
