140746ac9adb9f309585f3b97e55719039e32c2b
nijuy <youjin6325@naver.com>
2023-07-20 13:15:09 +0900

    feat(TextInput): props 추가 (type, pattern)

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../css/TextInput.css';
 
-const TextInput = ({ label, placeholder, onChange }) => {
+const TextInput = ({ label, placeholder, type, pattern, onChange }) => {
   return (
     <div id="input-container">
       <p>{label}</p>
-      <input placeholder={placeholder} onChange={onChange} />
+      <input placeholder={placeholder} type={type} pattern={pattern} onChange={onChange} />
     </div>
   );
 };
 
 TextInput.defaultProps = {
   placeholder: '',
+  type: '',
+  pattern: null,
 };
 
 export default TextInput;
