762eb405e4dd12ce6abde716fb1e87f3c52faac3
gyueunnim <rnrmfrbdms@gmail.com>
2023-07-20 20:06:56 +0900

    chore: firebase Config 데이터 설정

---
+++ src/data/firebaseConfig.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsgj_iDkXgoipFY9AUbuYNKQ1O7BiLcho',
+  authDomain: 'react-push-test-58035.firebaseapp.com',
+  projectId: 'react-push-test-58035',
+  storageBucket: 'react-push-test-58035.appspot.com',
+  messagingSenderId: '644484203614',
+  appId: '1:644484203614:web:04b2927d12fdf1e592a0cf',
+  measurementId: 'G-KLHNKZPZ4K',
+};
+
+const firebaseVapidKey =
+  'BI-_sWLOnaahPCqAvw8zFydQz1yR24QkOp817njCh_VWqLCGjdSbr3MtvjvHqbrz8D1cBXKdB8rab5O3hDoTTmk';
+
+export { firebaseConfig, firebaseVapidKey };
