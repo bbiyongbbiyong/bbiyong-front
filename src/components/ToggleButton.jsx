8b6535c70962c8ef362caca7b52ed4f407586348
nijuy <youjin6325@naver.com>
2023-07-21 19:15:29 +0900

    feat(Notification): OptionButton 컴포넌트 적용

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,23 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import OptionButton from './OptionButton';
 import '../css/ToggleButton.css';
 
-const ToggleButton = ({ label, onChange, index }) => {
-  const [checked, setChecked] = useState(true);
+const ToggleButton = ({ label, onChange, index, options }) => {
+  const [checkedOption, setCheckedOption] = useState(options);
+  const checked = checkedOption.length > 0;
 
-  const onChangeToggle = () => {
-    setChecked(!checked);
+  const onChangeToggle = (e) => {
+    if (e.target.checked) {
+      setCheckedOption(options);
+    } else {
+      setCheckedOption([]);
+    }
     onChange(index);
   };
 
+  const onChangeOption = (optionChecked, optionIndex) => {
+    if (optionChecked) {
+      setCheckedOption([...checkedOption, options[optionIndex]]);
+    } else {
+      setCheckedOption(checkedOption.filter((option) => option !== options[optionIndex]));
+    }
+  };
+
+  useEffect(() => {
+    if (!checked) onChange(index);
+  }, [checked]);
+
   return (
-    <div id="toggle-container">
-      <p> {label} </p>
-      <input type="checkbox" onChange={onChangeToggle} id={label} checked={checked} hidden />
-      <label htmlFor={label} className="toggleSwitch">
-        <span className="toggleButton"></span>
-      </label>
-    </div>
+    <>
+      <div id="toggle-container">
+        <p> {label} </p>
+        <input type="checkbox" onChange={onChangeToggle} id={label} checked={checked} hidden />
+        <label htmlFor={label} className="toggleSwitch">
+          <span className="toggleButton"></span>
+        </label>
+      </div>
+
+      <div id="option-container">
+        {options.map((option, optionIndex) => (
+          <OptionButton
+            key={optionIndex}
+            option={option}
+            index={optionIndex}
+            onChange={onChangeOption}
+            checked={checkedOption.includes(options[optionIndex])}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import notification from '../assets/notification.svg';
 import ToggleButton from '../components/ToggleButton';
@@ -7,6 +7,61 @@ import '../css/Notification.css';
 
 const Notification = () => {
   const MIDDLE_CATEGORY = ['자연재난', '사회재난', '지하철정보', '도로통제정보'];
+  const SUB_CATEGORY = [
+    [
+      '태풍',
+      '건조',
+      '산불',
+      '산사태',
+      '홍수',
+      '호우',
+      '폭염',
+      '안개',
+      '풍랑',
+      '미세먼지',
+      '대조기',
+      '가뭄',
+      '대설',
+      '지진해일',
+      '지진',
+      '한파',
+      '황사',
+      '강풍',
+      '기타 자연재난',
+    ],
+    [
+      '교통통제',
+      '화재',
+      '붕괴',
+      '폭발',
+      '교통사고',
+      '환경오염사고',
+      '에너지',
+      '통신',
+      '교통',
+      '금융',
+      '의료',
+      '수도',
+      '전염병',
+      '정전',
+      '가스',
+      '실종',
+      '기타 사회재난',
+    ],
+    [
+      '1호선',
+      '2호선',
+      '3호선',
+      '4호선',
+      '5호선',
+      '6호선',
+      '7호선',
+      '8호선',
+      '9호선',
+      '기타 지하철',
+    ],
+    ['도로사고', '집회/행사', '도로공사', '기타 도로통제'],
+  ];
 
   const [toggleStatus, setToggleStatus] = useState([true, true, true, true]);
   const notifyOn = toggleStatus.includes(true);
@@ -59,7 +114,13 @@ const Notification = () => {
 
       {notifyOn ? (
         MIDDLE_CATEGORY.map((category, index) => (
-          <ToggleButton key={index} label={category} onChange={handleToggleStatus} index={index} />
+          <ToggleButton
+            key={index}
+            label={category}
+            onChange={handleToggleStatus}
+            index={index}
+            options={SUB_CATEGORY[index]}
+          />
         ))
       ) : (
         <div> 현재 수신 받는 알림이 없습니다</div>
