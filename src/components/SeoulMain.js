a76e5b729442f219d79020930840144d2a61b457
gyueunnim <rnrmfrbdms@gmail.com>
2023-07-29 15:11:51 +0900

    feat: Chart 컴포넌트 적용

--- src/components/SeoulMain.js
+++ src/components/SeoulMain.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import '../css/message.css';
-import accidentType from '../data/accidentType.js';
 import { getMonth, getDay, getHours } from '../utils/dateUtil';
+
+import Chart from './Chart';
 
 export default function SeoulMain() {
   const year = new Date().getFullYear();
@@ -13,7 +14,6 @@ export default function SeoulMain() {
   const hours = getHours();
 
   // eslint-disable-next-line global-require
-  const mapData = require('../data/mapData.json').data;
 
   const [Accident, setAccident] = useState(null);
   const [, setLoading] = useState(false);
@@ -47,26 +47,12 @@ export default function SeoulMain() {
   if (Error) return <div>Error</div>;
   if (!Accident) return null;
 
-  const city = mapData[Accident.data.locationId - 2].SIG_KOR_NM;
-  const accidentName = accidentType[Accident.data.accidentType];
-
   return (
     <div className={`seoul-main start ${fade}`}>
       <p className="seoul-main-date">
         {year}년 {month}월 {day}일 {hours}시 기준
       </p>
-      <p>
-        지난 일주일 간<br />
-        <span className="seoul-main-city">{city}</span>에서{' '}
-        <span className="seoul-main-accident">{accidentName}</span>(이)가
-        <br />
-        가장 많이 발생했어요.
-      </p>
-      <p>
-        {city}를 지나실 때는
-        <br />
-        {accidentName}에 주의하세요 :)
-      </p>
+      <Chart />
     </div>
   );
 }
