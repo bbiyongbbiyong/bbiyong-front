5af69da0bafafd6ebf19528bd74d6ed030c6d8d9
gyueunnim <rnrmfrbdms@gmail.com>
2023-07-29 15:04:57 +0900

    feat: Chart 컴포넌트 추가

---
+++ src/components/Chart.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+import axios from 'axios';
+
+export default function Chart() {
+  const [analytics, setAnalytics] = useState([]);
+  const chartData = [
+    {
+      name: '지난주',
+      data: [analytics.emergencyMsgYdy, analytics.metroYdy, analytics.accidentYdy],
+      color: '#F43661',
+    },
+    {
+      name: '이번주',
+      data: [analytics.emergencyMsgTdy, analytics.metroTdy, analytics.accidentTdy],
+      color: '#3399FF',
+    },
+  ];
+
+  const chartSetting = {
+    options: {
+      chart: {
+        type: 'bar',
+        height: 430,
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          dataLabels: {
+            position: 'top',
+          },
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        offsetX: -6,
+        style: {
+          fontSize: '12px',
+          colors: ['#fff'],
+        },
+      },
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff'],
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+      },
+      xaxis: {
+        categories: ['재난문자', '지하철정보', '도로통제정보'],
+      },
+      yaxis: {
+        labels: {
+          align: 'left',
+          style: {
+            fontWeight: 'bold',
+            fontSize: '14px',
+          },
+          offsetX: -10,
+        },
+        min: 0,
+        max: 100,
+      },
+    },
+  };
+  const getChartInfo = async () => {
+    try {
+      const response = await axios.get('https://api.bbiyong-bbiyong.seoul.kr/analytics');
+      setAnalytics(response.data.data);
+    } catch (e) {
+      console.log('사고 유형 불러오기 실패');
+    }
+  };
+
+  useEffect(() => {
+    getChartInfo();
+  }, []);
+
+  return (
+    <ReactApexChart
+      options={chartSetting.options}
+      series={chartData}
+      type="bar"
+      height={250}
+      width={325}
+    />
+  );
+}
